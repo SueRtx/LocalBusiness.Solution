18832adf9fe00d64bfdbf5a48aedcc7a00589596
Sue Roberts <suerobertstx@yahoo.com>
2022-09-08 15:04:47 -0500

    Add GET: api/Businesses

---
+++ LocalApi/Controllers/BusinessesController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocalApi.Models;
+
+namespace LocalApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BusinessesController : ControllerBase
+  {
+    private readonly LocalApiContext _db;
+
+    public BusinessesController(LocalApiContext db)
+    {
+      _db = db;
+    }
+
+  // GET: api/Businesses
+    [HttpGet]
+    public async Task<List<Business>> Get(string name, string description, string location)
+    {
+      IQueryable<Business> query = _db.Businesss.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+
+      }
+      if (description != null)
+      {
+        query = query.Where(entry => entry.Description == description);
+      }
+
+      if (location != null)
+      {
+        query = query.Where(entry => entry.Location >= location);
+      }
+
+      return await query.ToListAsync();
+    }
+
+
+    
+  }
+}
