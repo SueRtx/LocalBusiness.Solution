47cf861a0185ba4ffc7ae5f81209d4b5c0e8cded
Sue Roberts <suerobertstx@yahoo.com>
2022-09-08 19:37:01 -0500

    Install Swashbuckle & update Startup.cs

--- LocalApi/Startup.cs
+++ LocalApi/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using LocalApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
 
 namespace LocalApi
 {
@@ -24,11 +28,44 @@ namespace LocalApi
             services.AddDbContext<LocalApiContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Version = "v1",
+                    Title = "Business API", 
+                    Description = "API  FOR BUSINESS LIST",
+        
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Sue Roberts",
+                        Email = "suerobertstx@yahoo.com",
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+        // Enable middleware to serve generated Swagger as a JSON endpoint.
+        app.UseSwagger();
+
+        // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+        // specifying the Swagger JSON endpoint.
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "BUSINESS API V1");
+            c.RoutePrefix = string.Empty;
+        });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
