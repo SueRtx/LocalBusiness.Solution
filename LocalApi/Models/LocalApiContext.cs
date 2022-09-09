93ea62526d4a3bf5f40429a7913d1af76042d50c
Sue Roberts <suerobertstx@yahoo.com>
2022-09-08 20:18:30 -0500

    Update LocalApiContext.cs

--- LocalApi/Models/LocalApiContext.cs
+++ LocalApi/Models/LocalApiContext.cs
@@ -13,11 +13,11 @@ namespace LocalApi.Models
         {
             builder.Entity<Business>()
             .HasData(
-                new Business { BusinessId = 1, Name = "Matilda", Description = "Woolly Mammoth", Location = "Baytown" },
-                new Business { BusinessId = 2, Name = "Rexie", Description = "Dinosaur", Location = "Baytown" },
-                new Business { BusinessId = 3, Name = "Matilda", Description = "Dinosaur", Location = "Baytown" },
-                new Business { BusinessId = 4, Name = "Pip", Description = "Shark", Location = "Baytown" },
-                new Business { BusinessId = 5, Name = "Bartholomew", Description = "Dinosaur", Location = "Baytown" }
+                new Business { BusinessId = 1, Name = "Matilda", Description = "Restaurant", Location = "Baytown" },
+                new Business { BusinessId = 2, Name = "Rexie", Description = "Shop", Location = "" },
+                new Business { BusinessId = 3, Name = "Blue Room", Description = "Shop", Location = "Beachcity" },
+                new Business { BusinessId = 4, Name = "Pipeline", Description = "Restaurant", Location = "Baytown" },
+                new Business { BusinessId = 5, Name = "Sweet Dream", Description = "Shop", Location = "Houston" }
             );
         }
         
