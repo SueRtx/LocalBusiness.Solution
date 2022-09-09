92302761c5341eded3f7e81208649842f0647d1c
Sue Roberts <suerobertstx@yahoo.com>
2022-09-08 19:55:38 -0500

    Add Required to Business.cs

--- LocalApi/Models/Business.cs
+++ LocalApi/Models/Business.cs
@@ -5,11 +5,12 @@ namespace LocalApi.Models
   public class Business
   {
     public int BusinessId { get; set; }
-    
+    [Required]
+    [StringLength(20)]
     public string Name { get; set; }
-    
+    [Required]
     public string Description { get; set; }
-    
+    [Required]
     public string Location { get; set; }
 
   }
