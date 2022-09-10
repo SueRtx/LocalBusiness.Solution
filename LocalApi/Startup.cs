fe2717c3fe12aa4a37e6bf8d68ef7f10a230aa2d
Sue Roberts <suerobertstx@yahoo.com>
2022-09-09 22:50:33 -0500

    WIP: Authorize does not work using Swagger

--- LocalApi/Startup.cs
+++ LocalApi/Startup.cs
@@ -61,14 +61,13 @@ namespace LocalApi
 
             services.AddControllers();
             
-            // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo 
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
                 { 
                     Version = "v1",
                     Title = "Business API", 
-                    Description = "API  FOR BUSINESS LIST",
+                    Description = "API FOR LOCAL BUSINESS LIST",
         
                     Contact = new OpenApiContact
                     {
@@ -76,6 +75,33 @@ namespace LocalApi
                         Email = "suerobertstx@yahoo.com",
                     }
                 });
+
+                // To Enable authorization using Swagger (JWT)  
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()  
+                {  
+                    Name = "Authorization",  
+                    Type = SecuritySchemeType.ApiKey,  
+                    Scheme = "Bearer",  
+                    BearerFormat = "JWT",  
+                    In = ParameterLocation.Header,  
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",  
+                });  
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement  
+                {  
+                    {  
+                          new OpenApiSecurityScheme  
+                            {  
+                                Reference = new OpenApiReference  
+                                {  
+                                    Type = ReferenceType.SecurityScheme,  
+                                    Id = "Bearer"  
+                                }  
+                            },  
+                            new string[] {}  
+  
+                    }  
+                });  
+              
 
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
